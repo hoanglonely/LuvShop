3e31e79921496445ee7649f395444fed04a78024
tvhoang <tvhdhv@gmail.com>
2019-01-14 23:04:39 +0700

    Model

---
+++ LuvShop.Model/Models/Product.cs
@@ -0,0 +1,33 @@
+﻿using LuvShop.Model.Abstract;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Linq;
+
+namespace LuvShop.Model.Models
+{
+    [Table("Products")]
+    public class Product : Audiable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Alias { get; set; }
+        public int CategoryID { get; set; }
+        public string Image { get; set; }
+        public XElement MoreImages { get; set; }
+        public decimal Price { get; set; }
+        public decimal? PromotionPrice { get; set; } //Gia khuyen mai
+        public int? Warranty { get; set; } //Bao hanh
+        public string Description { get; set; }
+        public string Content { get; set; }
+
+        public bool? HomeFlag { get; set; }
+        public bool? HotFlag { get; set; }
+        public int? ViewCount { get; set; }
+        [ForeignKey("CategoryID")]
+        public virtual ProductCategory ProductCategory { get; set; }
+    }
+}
---
+++ LuvShop.Model/Models/ProductCategory.cs
@@ -0,0 +1,25 @@
+﻿using LuvShop.Model.Abstract;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LuvShop.Model.Models
+{
+    [Table("ProductCategorys")]
+    public class ProductCategory : Audiable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Alias { get; set; }
+        public string Description { get; set; }
+        public int? ParentID { get; set; }
+        public int? DisplayOrder { get; set; }
+        public string Image { get; set; }
+        public bool? HomeFlag { get; set; }
+        public virtual IEnumerable<Product> Products { get; set; }//Ko co that, chi de tham chieu
+    }
+}
